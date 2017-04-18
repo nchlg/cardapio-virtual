35dcd61542ee3c3fe32d5477b8e3c6307e86f5bf
pris0213 <priscila.vivian@acad.pucrs.br>
2017-04-18 17:49:47 -0300

    Tela de detalhamento de restaurantes feita

--- app/src/main/java/facin/com/cardapio_virtual/MenuActivity.java
+++ app/src/main/java/facin/com/cardapio_virtual/MenuActivity.java
@@ -1,8 +1,10 @@
 package facin.com.cardapio_virtual;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.io.File;
 
@@ -13,16 +15,79 @@ public class MenuActivity extends AppCompatActivity
 
     public File lanchesFile;
 
+    // Intent
+    private String intentNome;
+    private String intentDescricao;
+    private String intentEndereco;
+    private String intentEmail;
+    private String intentTelefone;
+
+    private final static int REQUEST_RESTAURANT_INFO = 13;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        // Intent
+        Intent intent = getIntent();
+        if(intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_NOME) != null) {
+            intentNome = intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_NOME);
+        }
+        if (intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_DESCRICAO) != null) {
+            intentDescricao = intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_DESCRICAO);
+        } else {
+            intentDescricao = getResources().getString(R.string.indisp_description);
+        }
+        if (intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_ENDERECO) != null) {
+            intentEndereco = intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_ENDERECO);
+        } else {
+            intentEndereco = getResources().getString(R.string.indisp_address);
+        }
+        if (intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_EMAIL) != null) {
+            intentEmail = intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_EMAIL);
+        } else {
+            intentEmail = getResources().getString(R.string.indisp_email);
+        }
+        if (intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_TELEFONE) != null) {
+            intentTelefone = intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_TELEFONE);
+        } else {
+            intentTelefone = getResources().getString(R.string.indisp_phone_number);
+        }
     }
 
     @Override
     public void onListFragmentInteraction(Product product) {
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        setIntent();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home: {
+                setIntent();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
+    }
+
+    protected void setIntent() {
+        Intent intent = new Intent();
+        intent.putExtra(MainActivity.EXTRA_RESTAURANT_NOME, intentNome);
+        intent.putExtra(MainActivity.EXTRA_RESTAURANT_DESCRICAO, intentDescricao);
+        intent.putExtra(MainActivity.EXTRA_RESTAURANT_ENDERECO, intentEndereco);
+        intent.putExtra(MainActivity.EXTRA_RESTAURANT_EMAIL, intentEmail);
+        intent.putExtra(MainActivity.EXTRA_RESTAURANT_TELEFONE, intentTelefone);
+        // requestCode - int: If >= 0, this code will be returned in onActivityResult() when the activity exits
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
 
--- app/src/main/java/facin/com/cardapio_virtual/RestaurantInfoActivity.java
+++ app/src/main/java/facin/com/cardapio_virtual/RestaurantInfoActivity.java
@@ -3,8 +3,11 @@ package facin.com.cardapio_virtual;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.hp.hpl.jena.iri.impl.Main;
 
 public class RestaurantInfoActivity extends AppCompatActivity {
 
@@ -13,6 +16,12 @@ public class RestaurantInfoActivity extends AppCompatActivity {
     private TextView mEmailText;
     private TextView mTelefoneText;
     private Button mBtnMenu;
+
+    // Intent
+    private String intentDescricao;
+    private String intentEndereco;
+    private String intentEmail;
+    private String intentTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +38,68 @@ public class RestaurantInfoActivity extends AppCompatActivity {
         }
 
         // TextViews
+        setTextViewContent(intent);
+
+        // Buttons
+        mBtnMenu = (Button) findViewById(R.id.btn_cardapio);
+        mBtnMenu.setText(getResources().getString(R.string.btn_menu));
+        mBtnMenu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(RestaurantInfoActivity.this, MenuActivity.class);
+                intent.putExtra(MainActivity.EXTRA_RESTAURANT_DESCRICAO, intentDescricao);
+                intent.putExtra(MainActivity.EXTRA_RESTAURANT_ENDERECO, intentEndereco);
+                intent.putExtra(MainActivity.EXTRA_RESTAURANT_EMAIL, intentEmail);
+                intent.putExtra(MainActivity.EXTRA_RESTAURANT_TELEFONE, intentTelefone);
+                startActivityForResult(intent, 13);
+            }
+        });
+
+    }
+
+    protected void setTextViewContent(Intent intent) {
         mDescricaoText = (TextView) findViewById(R.id.descricao_text);
         mEnderecoText = (TextView) findViewById(R.id.endereco_text);
         mEmailText = (TextView) findViewById(R.id.email_text);
         mTelefoneText = (TextView) findViewById(R.id.telefone_text);
         // Popula os campos de texto com os dados do restuarante
-        mDescricaoText.setText(intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_DESCRICAO) != null ?
-                intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_DESCRICAO) :
-                getResources().getString(R.string.indisp_description));
+        if (intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_DESCRICAO) != null) {
+            intentDescricao = intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_DESCRICAO);
+        } else {
+            intentDescricao = getResources().getString(R.string.indisp_description);
+        }
+        mDescricaoText.setText(intentDescricao);
 
-        mEnderecoText.setText(intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_ENDERECO) != null ?
-                intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_ENDERECO) :
-                getResources().getString(R.string.indisp_address));
+        if (intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_ENDERECO) != null) {
+            intentEndereco = getResources().getString(R.string.pre_address) + ": " +
+                    intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_ENDERECO) + ".";
+        } else {
+            intentEndereco = getResources().getString(R.string.indisp_address);
+        }
+        mEnderecoText.setText(intentEndereco);
 
-        mEmailText.setText(intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_EMAIL) != null ?
-                intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_EMAIL) :
-                getResources().getString(R.string.indisp_email));
+        if (intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_EMAIL) != null) {
+            intentEmail = getResources().getString(R.string.pre_email) + ": " +
+                    intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_EMAIL) + ".";
+        } else {
+            intentEmail = getResources().getString(R.string.indisp_email);
+        }
+        mEmailText.setText(intentEmail);
 
-        mTelefoneText.setText(intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_TELEFONE) != null ?
-                intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_TELEFONE) :
-                getResources().getString(R.string.indisp_phone_number));
+        if (intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_TELEFONE) != null) {
+            intentTelefone = getResources().getString(R.string.pre_phone_number) + ": " +
+                    intent.getStringExtra(MainActivity.EXTRA_RESTAURANT_TELEFONE) + ".";
+        } else {
+            intentTelefone = getResources().getString(R.string.indisp_phone_number);
+        }
+        mTelefoneText.setText(intentTelefone);
+    }
 
-        // Buttons
-        mBtnMenu = (Button) findViewById(R.id.btn_cardapio);
-        mBtnMenu.setText(getResources().getString(R.string.btn_menu));
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 13) {
+            if (resultCode == RESULT_OK)
+                setTextViewContent(data);
+        }
     }
 }
